74efbd989a3ba7257912bee46f2a2007d636bf89
Kuckelkorn <remco.kuckelkorn@soverin.net>
2022-03-22 15:12:35 +0100

    set view engine

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const path = require('path')
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
@@ -9,13 +9,19 @@ let port = process.env.PORT || 5555
 const app = express()
 
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+//Initialise app
+app
+  .use('/static', express.static('./public/static'))
+  .set('view engine', 'pug')
+  .set('views', './server/views')
+  .use(bodyParser.urlencoded({ extended: true }))
 
 
 // Routes
 app
   .get('/', )
 
+// Start app
 app.listen(port, () => {
   console.log('Server started at port ' + port);
 });
