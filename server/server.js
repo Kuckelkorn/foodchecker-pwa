f3fb94d3501d8d6a86d83a8e73e3808bd3110e39
Kuckelkorn <remco.kuckelkorn@soverin.net>
2022-03-22 15:06:29 +0100

    isntall pug

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const path = require('path')
+
 require('dotenv').config();
 
 let port = process.env.PORT || 5555
 
 
 const app = express()
+
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+
+// Routes
+app
+  .get('/', )
 
 app.listen(port, () => {
   console.log('Server started at port ' + port);
