c036d6d46f589c7900593d1764b3332b5b95038b
Kuckelkorn <remco.kuckelkorn@soverin.net>
2022-04-05 14:12:15 +0200

    minify css

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CORE_CACHE = ['/offline', 'style-v2.css', '/', '/camera', 'scripts/camera-v2.min.js', 'https://fonts.googleapis.com', 'https://fonts.gstatic.com', 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap', 'https://kit.fontawesome.com/154837199a.js']
-const core_name = 'core-cache-v3'
+const CORE_CACHE = ['/offline', 'style-v2.min.css', '/', '/camera', 'scripts/camera-v2.min.js', 'https://fonts.googleapis.com', 'https://fonts.gstatic.com', 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap', 'https://kit.fontawesome.com/154837199a.js']
+const core_name = 'core-cache-v4'
 
 self.addEventListener('install', (e) => {
   console.log('installed')
